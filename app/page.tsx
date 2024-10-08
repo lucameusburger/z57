26caaac15dd2efd4f23e8452458356eb9cc41c9d
Luca Meusburger <meusburger.luca@gmail.com>
2024-10-08 20:44:02 +0200

    asd

--- app/page.tsx
+++ app/page.tsx
@@ -311,7 +311,7 @@ export default function Home() {
             </div>
           </div>
         </footer>
-        <CustomCursor />
+
         {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a className="flex items-center gap-2 hover:underline hover:underline-offset-4" href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
           <Image aria-hidden src="https://nextjs.org/icons/file.svg" alt="File icon" width={16} height={16} />
